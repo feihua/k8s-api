85e05f3ee7daaef6982d545881773687b0f0c1f4
koobe <koobe.liu@uaf.com.cn>
2021-07-08 15:01:15 +0800

    delete add method

--- internal/logic/namespace/namespacecreatelogic.go
+++ internal/logic/namespace/namespacecreatelogic.go
@@ -2,6 +2,12 @@ package logic
 
 import (
 	"context"
+	"fmt"
+	apiV1 "k8s.io/api/core/v1"
+	metaV1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s_test/internal/common/errorx"
 
 	"k8s_test/internal/svc"
 	"k8s_test/internal/types"
@@ -24,7 +30,35 @@ func NewNamespaceCreateLogic(ctx context.Context, svcCtx *svc.ServiceContext) Na
 }
 
 func (l *NamespaceCreateLogic) NamespaceCreate(req types.NamespaceAddReq) (*types.NamespaceAddResp, error) {
-	// todo: add your logic here and delete this line
+	// k8s 配置
+	kubeConfig := "etc/config"
+	config, err := clientcmd.BuildConfigFromFlags("", kubeConfig)
 
-	return &types.NamespaceAddResp{}, nil
+	if err != nil {
+		return nil, errorx.NewDefaultError(err.Error())
+	}
+	clientSet, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		return nil, errorx.NewDefaultError(err.Error())
+	}
+
+	namespaceClient := clientSet.CoreV1().Namespaces()
+	namespace := &apiV1.Namespace{
+		ObjectMeta: metaV1.ObjectMeta{
+			Name: req.Name,
+		},
+		Status: apiV1.NamespaceStatus{
+			Phase: apiV1.NamespaceActive,
+		},
+	}
+	result, err := namespaceClient.Create(context.TODO(), namespace, metaV1.CreateOptions{})
+	if err != nil {
+		return nil, errorx.NewDefaultError(err.Error())
+	}
+
+	fmt.Println(result)
+	return &types.NamespaceAddResp{
+		Code: 0,
+		Msg:  "successful",
+	}, nil
 }
--- internal/logic/serve/servicecreatelogic.go
+++ internal/logic/serve/servicecreatelogic.go
@@ -2,6 +2,12 @@ package logic
 
 import (
 	"context"
+	"fmt"
+	coreV1 "k8s.io/api/core/v1"
+	metaV1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+	"log"
 
 	"k8s_test/internal/svc"
 	"k8s_test/internal/types"
@@ -24,7 +30,49 @@ func NewServiceCreateLogic(ctx context.Context, svcCtx *svc.ServiceContext) Serv
 }
 
 func (l *ServiceCreateLogic) ServiceCreate(req types.ServiceAddReq) (*types.ServiceAddResp, error) {
-	// todo: add your logic here and delete this line
+	// k8s 配置
+	kubeConfig := "etc/config"
+	config, err := clientcmd.BuildConfigFromFlags("", kubeConfig)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// 6. service 创建
+	serviceClient := clientset.CoreV1().Services("default")
+	service := &coreV1.Service{
+		ObjectMeta: metaV1.ObjectMeta{
+			Name: "test-nginx-dev",
+			Labels: map[string]string{
+				"source": "cmdb",
+				"app":    "nginx",
+				"env":    "test",
+			},
+		},
+		Spec: coreV1.ServiceSpec{
+			Selector: map[string]string{
+				"source": "cmdb",
+				"app":    "nginx",
+				"env":    "test",
+			},
+			Type: coreV1.ServiceTypeNodePort,
+			Ports: []coreV1.ServicePort{
+				{
+					Name:     "http",
+					Port:     80,
+					Protocol: coreV1.ProtocolTCP,
+				},
+			},
+		},
+	}
+	service, err = serviceClient.Create(context.TODO(), service, metaV1.CreateOptions{})
+	if err != nil {
+		fmt.Println(err)
+	}
 
 	return &types.ServiceAddResp{}, nil
 }
