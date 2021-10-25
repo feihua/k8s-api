7ddac8d170c31204316c31d1ed856b1a485f012e
koobe <koobe.liu@uaf.com.cn>
2021-10-25 17:59:16 +0800

    添加jenkins连接

--- go.mod
+++ go.mod
@@ -3,11 +3,13 @@ module k8s_test
 go 1.14
 
 require (
+	github.com/bndr/gojenkins v1.1.0
 	github.com/gorilla/websocket v1.4.2
 	github.com/kr/text v0.2.0 // indirect
 	github.com/owenliang/k8s-client-go v0.0.0-20190606090414-8bde811a1146
 	github.com/tal-tech/go-zero v1.2.2
 	go.uber.org/automaxprocs v1.4.0 // indirect
+	golang.org/x/net v0.0.0-20211020060615-d418f374d309 // indirect
 	gorm.io/driver/mysql v1.1.2
 	gorm.io/gorm v1.21.16
 	k8s.io/api v0.20.10
--- internal/svc/servicecontext.go
+++ internal/svc/servicecontext.go
@@ -2,6 +2,7 @@ package svc
 
 import (
 	"context"
+	"github.com/bndr/gojenkins"
 	"github.com/tal-tech/go-zero/core/logx"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
@@ -20,6 +21,7 @@ type ServiceContext struct {
 	ClientSet *kubernetes.Clientset
 	DbClient  *gorm.DB
 	RestConf  *rest.Config
+	Jenkins   *gojenkins.Jenkins
 }
 
 func NewServiceContext(c config.Config) *ServiceContext {
@@ -35,6 +37,8 @@ func NewServiceContext(c config.Config) *ServiceContext {
 		logx.Errorf("连接kubernetes：%s异常: %s", kubeConfig, err.Error())
 	}
 
+	logx.Info("连接kubernetes成功")
+
 	//启动Gorm支持
 	db, err := gorm.Open(mysql.Open(c.Mysql.Datasource), &gorm.Config{
 		NamingStrategy: schema.NamingStrategy{
@@ -47,14 +51,26 @@ func NewServiceContext(c config.Config) *ServiceContext {
 	if err != nil {
 		panic(err)
 	}
+	logx.Info("连接Mysql成功")
 	//自动同步更新表结构,不要建表了O(∩_∩)O哈哈~
 	//db.AutoMigrate(&models.User{})
+
+	jenkins := gojenkins.CreateJenkins(nil, "http://10.168.12.132:9093/jenkins", "admin", "admin@UAT18")
+
+	ctx := context.Background()
+	_, err = jenkins.Init(ctx)
+	if err != nil {
+		logx.Errorf("连接Jenkins失败: %s", err.Error())
+	}
+
+	logx.Info("连接Jenkins成功")
 
 	return &ServiceContext{
 		Config:    c,
 		ClientSet: clientSet,
 		DbClient:  db,
 		RestConf:  buildConfigFromFlags,
+		Jenkins:   jenkins,
 	}
 }
 
